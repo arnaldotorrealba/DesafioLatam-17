daaea283c626c0de7694f912cf443ae3c0a5cf67
arnaldotorr <arnaldo.torrealba@gmail.com>
2023-09-28 14:46:35 -0300

    First Commit

---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+const petData = [
+    {
+        id: 1,
+        name: "Luna",
+        description:
+            "Una perrita enérgica y juguetona que adora correr y jugar al aire libre. Es ideal para familias activas que disfrutan de la vida al aire libre.",
+        imgUrl: "https://cdn.pixabay.com/photo/2021/08/18/22/42/australian-shepherd-6556697_1280.jpg",
+        breed: "Pastor Australiano",
+        badgeBg: "primary",
+    },
+    {
+        id: 2,
+        name: "Coco",
+        description:
+            "Un perro amigable y tranquilo que siempre está dispuesto a recibir cariño y afecto. Es el compañero perfecto para quienes buscan un amigo relajado.",
+        imgUrl: "https://cdn.pixabay.com/photo/2022/02/09/20/52/labrador-retriever-7004193_640.jpg",
+        breed: "Labrador Retriever",
+        badgeBg: "success",
+    },
+    {
+        id: 3,
+        name: "Max",
+        description:
+            "Un perro activo y aventurero que disfruta de largos paseos y juegos al aire libre. Acompaña a personas activas y enérgicas en sus aventuras diarias.",
+        imgUrl: "https://cdn.pixabay.com/photo/2021/01/21/16/17/english-cocker-spaniel-5937757_1280.jpg",
+        breed: "Cocker Spaniel Inglés",
+        badgeBg: "danger",
+    },
+    {
+        id: 4,
+        name: "Bella",
+        description:
+            "Una perra inteligente y cariñosa que siempre está lista para aprender nuevos trucos. Es la compañera perfecta para quienes buscan desafíos emocionantes.",
+        imgUrl: "https://cdn.pixabay.com/photo/2019/04/13/00/20/dog-4123618_640.jpg",
+        breed: "Tejonero",
+        badgeBg: "warning",
+    },
+    {
+        id: 5,
+        name: "Charlie",
+        description:
+            "Un perro cariñoso y leal que disfruta estar rodeado de amigos y familiares. Siempre te querrá incondicionalmente.",
+        imgUrl: "https://cdn.pixabay.com/photo/2017/06/24/09/13/continental-bulldog-2437110_1280.jpg",
+        breed: "Bulldog",
+        badgeBg: "success",
+    },
+    {
+        id: 6,
+        name: "Rex",
+        description:
+            "Un perro cariñoso e inteligente que también es un excelente guardián de su hogar. Siempre te protegerá y te amará incondicionalmente.",
+        imgUrl: "https://cdn.pixabay.com/photo/2016/01/05/17/51/maltese-1123016_640.jpg",
+        breed: "Maltés",
+        badgeBg: "primary",
+    },
+];
+
+export default petData;
